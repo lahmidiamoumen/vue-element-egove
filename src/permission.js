83efb87246fed8524751ecca028709408615f72c
Moumen lahmidi <a.lahmidi@univ-boumerdes.dz>
2023-10-24 22:06:52 +0100

    fix: fix router

--- src/permission.js
+++ src/permission.js
@@ -5,6 +5,7 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
+import Router from 'vue-router'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
@@ -38,6 +39,7 @@ router.beforeEach(async(to, from, next) => {
           // const roles = ['admin']
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', ['admin'])
+          router.match = new Router().matcher
 
           // dynamically add accessible routes
           router.addRoutes(accessRoutes)
