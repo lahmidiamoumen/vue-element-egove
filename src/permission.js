52e1cc808239a2fa40d2ec91ae79429c8b742030
Moumen lahmidi <a.lahmidi@univ-boumerdes.dz>
2023-10-24 22:31:18 +0100

    fix

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,7 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
-import Router from 'vue-router'
+// import Router from 'vue-router'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
@@ -38,11 +38,10 @@ router.beforeEach(async(to, from, next) => {
           // let roles = await store.dispatch('user/getInfo')
           // const roles = ['admin']
           // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', ['admin'])
-          router.matcher = new Router().matcher
+          // const accessRoutes = await store.dispatch('permission/generateRoutes', ['admin'])
 
           // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
+          // router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
