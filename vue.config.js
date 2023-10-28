88fbdfc2d968a5c515c454f9fb8366a3a1ee3418
Moumen lahmidi <a.lahmidi@univ-boumerdes.dz>
2023-10-28 13:23:34 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: process.env.NODE_ENV === 'development' ? 'http://localhost:9000/' : ''
+        target: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : ''
       }
     },
     port: port,
