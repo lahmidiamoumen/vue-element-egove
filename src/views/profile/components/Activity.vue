86e745dd2e4e02c67404f598ef7d91b2aa9495dd
Moumen lahmidi <a.lahmidi@univ-boumerdes.dz>
2023-10-28 11:54:03 +0100

    fixed

--- src/views/profile/components/Activity.vue
+++ src/views/profile/components/Activity.vue
@@ -462,7 +462,7 @@ export default {
   },
   methods: {
     async getBalanced() {
-      const b = this.ethAddress !== '' ? (Number(await this.web3.eth.getBalance(this.ethAddress)) * Math.pow(10, -9)).toFixed(4) : ''
+      const b = this.ethAddress !== '' ? (Number(await this.web3.eth.getBalance(this.ethAddress)) * Math.pow(10, -18)).toFixed(4) : ''
       this.balance = b
       return b
     },
